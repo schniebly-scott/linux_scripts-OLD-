6fde54bc724673eb371228a9c289074dba1ec3d1
schniebly-scott <atsimcode@gmail.com>
2021-08-19 22:12:28 -0500

    vieo codecs

--- quick_setup.sh
+++ quick_setup.sh
@@ -9,7 +9,8 @@ cd ..
 sudo rm -r yay
 
 #-----install all packages----------
-yay -S --noconfirm xorg-server xorg-xinit man kitty polybar pnmixer blueman network-manager-applet remmina freerdp vncproto picom-jonaburg-git etherwake net-tools nmap spotify vivaldi-stable xbindkeys python ruby playerctl nitrogen gucharmap grub-customizer git discord bc xorg-xrandr alsa-utils nerd-fonts-complete vlc inkscape wine virtualbox lightdm lightdm-webkit-theme-aether papirus-icon-theme i3-gaps xsslock i3lock-fancy rofi octopi libvncserver powerkit eog xfce4-power-manager gnome-image-viewer filezilla windscribe-cli zoom zscroll-git metasploit usbutils gvim linux-lts linux-lts-headers a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gstreamer0.10-plugins p7zip p7zip-plugins unrar tar rsync lxappearance
+yay -S --noconfirm xorg-server xorg-xinit libxft libxinerama man kitty polybar pnmixer blueman network-manager-applet remmina freerdp vncproto picom-jonaburg-git etherwake net-tools nmap spotify vivaldi-stable xbindkeys python ruby playerctl nitrogen gucharmap grub-customizer git discord bc xorg-xrandr alsa-utils nerd-fonts-complete vlc inkscape wine virtualbox lightdm lightdm-webkit-theme-aether papirus-icon-theme i3-gaps xsslock i3lock-fancy rofi octopi libvncserver powerkit eog xfce4-power-manager gnome-image-viewer filezilla windscribe-cli zoom zscroll-git metasploit usbutils gvim linux-lts linux-lts-headers a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gstreamer0.10-plugins p7zip p7zip-plugins unrar tar rsync lxappearance thunar lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader
+
 
 #custom rofi install-----------------
 git clone --depth=1 https://github.com/adi1090x/rofi.git
